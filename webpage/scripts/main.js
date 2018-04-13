c36c19d0ed291b0a986cbc488a928ceba39e0111
mariomrtsnz <mario.mrtsnz@gmail.com>
2018-04-14 00:46:04 +0200

    Login and signup Ajax

--- webpage/scripts/main.js
+++ webpage/scripts/main.js
@@ -1,6 +1,9 @@
 window.onload = function init() {
-    var button = document.getElementById("signUp");
-    button.addEventListener("click", loadSignUp());
+    var signUp = document.getElementById("signUp");
+    signUp.addEventListener("click", loadSignUp);
+
+    var logIn = document.getElementById("logIn");
+    logIn.addEventListener("click", loadLogIn);
 
     function loadSignUp() {
         var xhttp = new XMLHttpRequest();
@@ -11,6 +14,18 @@ window.onload = function init() {
             }
         };
         xhttp.open("GET", "../html/signup.html", true);
+        xhttp.send();
+    }
+
+    function loadLogIn() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("form").innerHTML =
+                    this.responseText;
+            }
+        };
+        xhttp.open("GET", "../html/login.html", true);
         xhttp.send();
     }
 }
