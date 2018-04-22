58accb7f75e2f313488e17af176af664a61ab69c
mariomrtsnz <mario.mrtsnz@gmail.com>
2018-04-22 19:55:11 +0200

    Added dashboard profile submenu.

--- webpage/scripts/dashboard.js
+++ webpage/scripts/dashboard.js
@@ -1,10 +1,22 @@
 window.onload = function init() {
     var navToggle = document.getElementsByClassName('nav-toggle')[0];
     var navSide = document.getElementsByClassName('nav-side')[0];
+    var profileMenu = document.getElementById('profileMenu');
+    var profileSubMenu = document.getElementById('profileSubMenu');
+    profileMenu.addEventListener("click", toggleProfileSubMenu);
     navToggle.addEventListener("click", toggleSideBar);
 
     function toggleSideBar() {
         navSide.classList.toggle('nav-open');
+    }
+
+    function toggleProfileSubMenu() {
+        profileMenu.classList.toggle('profileSubMenu-toggle');
+        if (profileSubMenu.style.display == 'block') {
+            profileSubMenu.style.display = 'none';
+        } else {
+            profileSubMenu.style.display = 'block';
+        }
     }
 
 }
