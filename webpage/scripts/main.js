40e73941453526ac9b88459118f4f6f09d9bd7fc
mariomrtsnz <mario.mrtsnz@gmail.com>
2018-04-29 19:10:46 +0200

    Removed calendar style into its own scss file and index styling.

--- webpage/scripts/main.js
+++ webpage/scripts/main.js
@@ -4,33 +4,31 @@ window.onload = function init() {
 
     var logIn = document.getElementById("logIn");
     logIn.addEventListener("click", loadLogIn);
+}
+function loadSignUp() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("form").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "../html/signup.html", true);
+    xhttp.send();
+}
 
-    function loadSignUp() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("form").innerHTML =
-                    this.responseText;
-            }
-        };
-        xhttp.open("GET", "../html/signup.html", true);
-        xhttp.send();
-    }
+function loadLogIn() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("form").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "../html/login.html", true);
+    xhttp.send();
+}
 
-    function loadLogIn() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("form").innerHTML =
-                    this.responseText;
-            }
-        };
-        xhttp.open("GET", "../html/login.html", true);
-        xhttp.send();
-    }
-
-    function validateSignUp() {
-
-    }
+function validateSignUp() {
 
 }
