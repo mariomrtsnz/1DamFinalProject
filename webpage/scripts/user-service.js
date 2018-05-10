7bbc8dbd6110e18a1c6da05390a4ddb32d139c04
mariomrtsnz <mario.mrtsnz@gmail.com>
2018-05-10 21:29:49 +0200

    Added Ajax on Appointment modal to go Back and edit appointment data

--- webpage/scripts/user-service.js
+++ webpage/scripts/user-service.js
@@ -41,8 +41,26 @@ function loadConfirmation() {
         xhttp.open("GET", "../html/appointment-confirmation.html", true);
         xhttp.send();
         $(".appointment-content").css({
-            "grid-template-areas": '"date" "calendar" "time" "timePicker" "price" "finalPrice" "payNow" "payPhysically" "payLater"',
-            "grid-template-rows": "0.1fr 0.5fr 0.1fr 0.5fr 0.1fr 0.5fr 0.2fr 0.2fr 0.2fr"
+            "grid-template-areas": '"date" "calendar" "time" "timePicker" "price" "finalPrice" "payNow" "payPhysically" "payLater" "goBack"',
+            "grid-template-rows": "0.1fr 0.5fr 0.1fr 0.5fr 0.1fr 0.5fr 0.2fr 0.2fr 0.2fr 0fr"
         });
+        $("#continue").hide();
     }
+}
+
+function goBack() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementsByClassName("appointment-content")[0].innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "../html/appointment-initial.html", true);
+    xhttp.send();
+    $(".appointment-content").css({
+        "grid-template-areas": '"date" "calendar" "time" "timePicker"',
+        "grid-template-rows": "0.2fr 1fr 0.2fr 1fr"
+    });
+    $("#continue").show();
 }
