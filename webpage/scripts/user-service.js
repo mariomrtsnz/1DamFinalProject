946628c66d2b4eea7b731afa3278b417c6627434
Mario Martinez <mario.mrtsnz@gmail.com>
2018-05-10 14:27:11 +0200

    Added modal continue button listener

--- webpage/scripts/user-service.js
+++ webpage/scripts/user-service.js
@@ -1,7 +1,11 @@
 window.onload = function() {
     document.getElementById('exitButton').addEventListener('click', modalToggler);
     document.getElementById('appointmentRequestButton').addEventListener('click', modalToggler);
+    document.getElementById('continue').addEventListener('click', loadConfirmation);
 }
 function modalToggler() {
     $('#appointmentModal').toggle('slow');
+}
+function loadConfirmation() {
+    /*Cargar AJAX de página de confirmación*/
 }
