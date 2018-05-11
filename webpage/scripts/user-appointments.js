bec57e8258b583497548fa7698204494b380ff9a
mariomrtsnz <mario.mrtsnz@gmail.com>
2018-05-11 23:06:36 +0200

    Fixed layout on table and table header.

--- webpage/scripts/user-appointments.js
+++ webpage/scripts/user-appointments.js
@@ -2,10 +2,4 @@ $(document).ready(function () {
     $('.search-button').click(function () {
         $(this).parent().toggleClass('open');
     });
-    $(window).on("load resize ", function () {
-        var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-        $('.tbl-header').css({
-            'padding-right': scrollWidth
-        });
-    }).resize();
 });
