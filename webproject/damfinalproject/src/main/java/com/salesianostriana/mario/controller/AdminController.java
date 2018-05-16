a348dbadca28c9d7ddb9115ff2a4e8bfd341b5c4
mariomrtsnz <mario.mrtsnz@gmail.com>
2018-05-16 23:03:56 +0200

    Started using Thymeleaf Layouts

--- webproject/damfinalproject/src/main/java/com/salesianostriana/mario/controller/AdminController.java
+++ webproject/damfinalproject/src/main/java/com/salesianostriana/mario/controller/AdminController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
-	@GetMapping("/admin")
+	@GetMapping({ "/admin", "/admin-dashboard-index.html" })
 	public String a() {
 		return "/admin/admin-dashboard-index";
 	}
