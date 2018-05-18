a81331121007b9897d2f000b0c5375ab522049cf
Mario Martinez <mario.mrtsnz@gmail.com>
2018-05-18 12:31:23 +0200

    Added dynamic data on user-service and fixed dynamic linking on specific products.

--- webproject/damfinalproject/src/main/java/com/salesianostriana/mario/controller/TreatmentController.java
+++ webproject/damfinalproject/src/main/java/com/salesianostriana/mario/controller/TreatmentController.java
@@ -1,17 +1,12 @@
 package com.salesianostriana.mario.controller;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
 
 import com.salesianostriana.mario.model.Treatment;
 import com.salesianostriana.mario.service.TreatmentService;
@@ -19,21 +14,28 @@ import com.salesianostriana.mario.service.TreatmentService;
 @Controller
 public class TreatmentController {
 
+	@Autowired
+	HttpSession session;
+	
+	@Autowired
 	TreatmentService treatmentService = new TreatmentService();
 
-	@GetMapping("/service/{id}")
-	public String serviceDetail(@PathVariable("id") long id, Model model) {
+	@GetMapping("/public/user-service/{id}")
+	public String serviceDetail(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("treatments", treatmentService.findAll());
+		model.addAttribute("loggedUser", session.getAttribute("loggedUser"));
+		
 		Treatment s = treatmentService.findOneById(id);
 		if (s != null) {
-			model.addAttribute("service", s);
-			return "ServiceDetail";
+			model.addAttribute("selectedTreatment", s);
+			return "/public/user-service";
 		} else {
 			// Tratamiento del error
 			return "Error";
 		}
 	}
 
-//	@PostMapping("/addService")
+//	@PostMapping("/addTreatment")
 //	public String submit(@ModelAttribute("FormService") Model model) {
 //		return "";
 //	}
