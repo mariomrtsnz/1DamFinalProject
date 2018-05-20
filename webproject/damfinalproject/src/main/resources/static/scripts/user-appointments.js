2c5aaeb290f4fb068730d2e15de77e65d8a34bd0
mariomrtsnz <mario.mrtsnz@gmail.com>
2018-05-20 14:35:48 +0200

    Hid Add Element modal and styled it.

--- webproject/damfinalproject/src/main/resources/static/scripts/user-appointments.js
+++ webproject/damfinalproject/src/main/resources/static/scripts/user-appointments.js
@@ -1,16 +1,6 @@
 $(document).ready(function () {
-    $('.search-button').click(function () {
-        $(this).parent().toggleClass('open');
-    });
-    $('tbody > tr').hover(function () {
-        var $advancedOptions = $(this).children('.advancedOptions').children('.dropdown').children('img');
-        $($advancedOptions).toggle();
-        $($advancedOptions).click(function () {
-            $(this).siblings('.dropdown-content').show();
-        });
-        $($advancedOptions).siblings('.dropdown-content').hover(function () {
-            $(this).toggle();
-        });
+    $('.btn.btn-default.footable-edit').click(function() {
+    	$('#editor-modal').show();
     });
     
     
