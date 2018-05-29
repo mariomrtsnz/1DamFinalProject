4720e4affd68995409e99e23039b73aec14c6341
Mario Martinez <mario.mrtsnz@gmail.com>
2018-05-29 12:15:28 +0200

    Implemented /error handling mapping on HomeController.

--- webproject/martinezmariodamfinalproject/src/main/java/com/salesianostriana/mario/controller/HomeController.java
+++ webproject/martinezmariodamfinalproject/src/main/java/com/salesianostriana/mario/controller/HomeController.java
@@ -3,11 +3,13 @@ package com.salesianostriana.mario.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class HomeController implements ErrorController{
 
 	@Autowired
 	HttpSession session;
@@ -17,4 +19,18 @@ public class HomeController {
 		session.setAttribute("loggedUser", null);
 		return "index";
 	}
+	
+	@RequestMapping("/error")
+    public String handleError() {
+        if (session.getAttribute("loggedUser") == null) {
+			return "redirect:/";
+		} else {
+			return "redirect:/public";
+		}
+    }
+ 
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
 }
